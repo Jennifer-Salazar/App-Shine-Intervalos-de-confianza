fdf68b56053e299be1e3f782c6545ffd13f872cb
Jennifer Salazar <65683869+Jennifer-Salazar@users.noreply.github.com>
2021-07-02 16:35:26 -0500

    Lectura base de datos estandar

--- Intervalos_confianza/server.R
+++ Intervalos_confianza/server.R
@@ -1,8 +1,14 @@
 
 library(shiny)
+data <- read.table("www/Ejercicio6.txt", sep=",", header = TRUE)
 
 shinyServer(function(input, output) {
-
+    
+    output$widget <- renderUI({
+        selectInput("etiquetas", "Seleccione la variable de interes",
+                    choices=colnames(data))
+        
+    })
 
 
 })
--- Intervalos_confianza/ui.R
+++ Intervalos_confianza/ui.R
@@ -5,8 +5,16 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    h1(strong("App para Intervalos de Confianza"), align= "center",
+       style="color: purple;font-family: cursive"), hr(),
+    
+    
+    navbarPage("App shiny",
+             wellPanel(uiOutput("widget")
+               
+    ))
+    
+   
 
 
 ))
